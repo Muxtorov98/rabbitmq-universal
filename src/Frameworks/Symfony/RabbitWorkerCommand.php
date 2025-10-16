f99d46d1067a8b336d47bcad03488b5a742631c3
Tulqin_Muxtorov98 <104984928+Muxtorov98@users.noreply.github.com>
2025-10-16 09:51:15 +0500

    fix: make Yii2 optional dependency to prevent bower-asset conflicts

--- src/Frameworks/Symfony/RabbitWorkerCommand.php
+++ src/Frameworks/Symfony/RabbitWorkerCommand.php
@@ -2,29 +2,37 @@
 
 namespace RabbitMQQueue\Frameworks\Symfony;
 
-use RabbitMQQueue\Core\{EnvLoader, RabbitMQConnection, HandlerRegistry, Worker};
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use RabbitMQQueue\Core\{EnvLoader, RabbitMQConnection, HandlerRegistry, Worker};
 
 class RabbitWorkerCommand extends Command
 {
     protected static $defaultName = 'rabbit:worker:start';
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('🚀 Loading environment...');
-        EnvLoader::load(__DIR__ . '/../../../');
 
+        // .env yuklash
+        $basePath = dirname(__DIR__, 3);
+        EnvLoader::load($basePath);
+
+        // RabbitMQ ulanadi
         $connection = new RabbitMQConnection();
         $connection->connect();
 
-        $path = $_ENV['HANDLER_PATH'] ?? __DIR__ . '/../../Handlers';
+        // Handlerlar ro‘yxatini yuklash
+        $path = $_ENV['HANDLER_PATH'] ?? $basePath . '/app/Handlers';
         $registry = new HandlerRegistry($path);
 
-        $output->writeln('👷 Worker started...');
+        $output->writeln("👷 Worker started and listening for messages...");
+
+        // Worker’ni ishga tushirish
         (new Worker($connection, $registry))->start();
 
+        // 🔥 Muhim: Symfony 5+ uchun `int` qaytarish majburiy
         return Command::SUCCESS;
     }
 }
